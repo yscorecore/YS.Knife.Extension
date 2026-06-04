58431ddd10b561938bc6c209e5fe5232cdb27f5c
yangsi <obpt@sina.com>
2026-06-04 14:06:01 +0800

    add token manager

---
+++ src/YS.Knife.TokenMan.Core/ITokenManager.cs
@@ -0,0 +1,35 @@
+﻿namespace YS.Knife.TokenMan
+{
+    public interface ITokenManager
+    {
+        /// <summary>
+        /// 获取Token，如果Token不存在或者过期，则调用tokenFun获取新的Token，并且根据expiredTimeFunc获取过期时间，保存Token到缓存中，返回Token。
+        /// </summary>
+        /// <typeparam name="T">Token的类型</typeparam>
+        /// <param name="name"> Token的名称</param>
+        /// <param name="tokenFactory">获取Token的委托函数</param>
+        /// <param name="expiredTimeFunc">Token的绝对过期时间</param>
+        /// <param name="bufferSeconds">缓冲时间，避免因网络延迟、时钟不同步导致 获取到的Token失效问题</param>
+        /// <param name="preloadSeconds">提前预加载的时间，在高并发的场景下，防止Token失效后大量请求等待一个线程请求Token。提前预加载会在Token失效前的一小段（去除了缓冲时间），某请求去请求Token，其余请求还使用旧的Token。提前预加载的时间要大于极端情况下的网络请求时间。</param>
+        /// <returns></returns>
+        Task<T> GetToken<T>(string name, Func<CancellationToken, Task<T>> tokenFactory, Func<T, DateTimeOffset> expiredTimeFunc, int bufferSeconds = 300, int preloadSeconds = 60, CancellationToken token = default) where T : class;
+    }
+
+    public static class ITokenManagerExtensions
+    {
+
+        public static Task<T> GetToken<T>(this ITokenManager tokenManager, string name, Func<CancellationToken, Task<T>> tokenFactory, Func<T, TimeSpan> expiredTimeFunc, int bufferSeconds = 300, int preloadSeconds = 60, CancellationToken token = default) where T : class
+        {
+            return tokenManager.GetToken(name, tokenFactory, t => DateTimeOffset.UtcNow.Add(expiredTimeFunc(t)), bufferSeconds, preloadSeconds, token);
+        }
+        public static Task<T> GetToken<T>(this ITokenManager tokenManager, string name, Func<CancellationToken, Task<T>> tokenFactory, TimeSpan expiredTime, int bufferSeconds = 300, int preloadSeconds = 60, CancellationToken token = default) where T : class
+        {
+            return tokenManager.GetToken(name, tokenFactory, t => expiredTime, bufferSeconds, preloadSeconds, token);
+        }
+        public static Task<T> GetToken<T>(this ITokenManager tokenManager, string name, Func<CancellationToken, Task<T>> tokenFactory, Func<T, DateTime> expiredTimeFunc, int bufferSeconds = 300, int preloadSeconds = 60, CancellationToken token = default) where T : class
+        {
+            return tokenManager.GetToken(name, tokenFactory, t => (DateTimeOffset)expiredTimeFunc(t), bufferSeconds, preloadSeconds, token);
+        }
+    }
+
+}
---
+++ src/YS.Knife.TokenMan.Impl.Default/TokenManager.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using YS.Knife.Lock;
+
+namespace YS.Knife.TokenMan.Impl.Default
+{
+    [Service]
+    [AutoConstructor]
+    public partial class TokenManager : ITokenManager
+    {
+        private readonly IDistributedCache cache;
+        private readonly ILockService lockService;
+        private readonly ILogger<TokenManager> logger;
+        private const string CacheKeyPrefix = "TokenMan:";
+
+        private const int MaxLockSeconds = 120;
+        private static JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+        {
+
+        };
+        public async Task<T> GetToken<T>(string name, Func<CancellationToken, Task<T>> tokenFun, Func<T, DateTimeOffset> expiredTimeFunc, int bufferSeconds = 300, int preloadSeconds = 60, CancellationToken cancellationToken = default)
+            where T : class
+        {
+            var key = $"{CacheKeyPrefix}{name}";
+            var value = await cache.GetObjectAsync<T>(key, JsonOptions);
+            if (value == null)
+            {
+                return await GetNewTokenWithLock(key, tokenFun, expiredTimeFunc, bufferSeconds, cancellationToken);
+            }
+            else
+            {
+                var expiredTime = expiredTimeFunc(value).AddSeconds(-bufferSeconds);
+                if ((expiredTime - DateTimeOffset.Now).TotalSeconds < preloadSeconds)
+                {
+                    //需要刷新
+                    return await TryRefreshTokenWithLock(key, tokenFun, expiredTimeFunc, bufferSeconds, value, cancellationToken) ?? value;
+                }
+                else
+                {
+                    return value;
+                }
+            }
+        }
+        private async Task<T> GetNewTokenWithLock<T>(string key, Func<CancellationToken, Task<T>> tokenFun, Func<T, DateTimeOffset> expiredTimeFunc, int bufferSeconds, CancellationToken cancellationToken)
+                where T : class
+        {
+            var (lockKey, lockAcquired) = ($"lock:{key}", false);
+            try
+            {
+                lockAcquired = await lockService.Lock(lockKey, TimeSpan.FromSeconds(MaxLockSeconds));
+                if (lockAcquired)
+                {
+                    var cacheVal = await cache.GetObjectAsync<T>(key, JsonOptions);
+                    if (cacheVal != null)
+                    {
+                        return cacheVal;
+                    }
+                    var newValue = await tokenFun(cancellationToken);
+
+                    var expiredTime = expiredTimeFunc(newValue).AddSeconds(-bufferSeconds);
+                    await cache.SetObjectAsync(key, newValue, new DistributedCacheEntryOptions { AbsoluteExpiration = expiredTime }, JsonOptions);
+                    logger.LogInformation("Token updated for key '{key}'", key);
+                    return newValue;
+                }
+                else
+                {
+                    await lockService.WaitFor(lockKey);
+                    var cachedValue = await cache.GetObjectAsync<T>(key, JsonOptions);
+                    return cachedValue ?? throw new InvalidOperationException($"Token for {key} not found after waiting for lock");
+                }
+            }
+            finally
+            {
+                if (lockAcquired)
+                {
+                    await lockService.UnLock(lockKey);
+                }
+            }
+        }
+        private async Task<T> TryRefreshTokenWithLock<T>(string key, Func<CancellationToken, Task<T>> tokenFun, Func<T, DateTimeOffset> expiredTimeFunc, int bufferSeconds, T defaultValue, CancellationToken cancellationToken)
+                where T : class
+        {
+            var (lockKey, lockAcquired) = ($"lock:{key}", false);
+            try
+            {
+                lockAcquired = await lockService.Lock(lockKey, TimeSpan.FromSeconds(MaxLockSeconds));
+                if (lockAcquired)
+                {
+                    var newValue = await tokenFun(cancellationToken);
+                    var expiredTime = expiredTimeFunc(newValue).AddSeconds(-bufferSeconds);
+                    await cache.SetObjectAsync(key, newValue, new DistributedCacheEntryOptions { AbsoluteExpiration = expiredTime }, JsonOptions);
+                    logger.LogInformation("Token updated for key '{key}'", key);
+                    return newValue;
+                }
+                else
+                {
+                    return defaultValue;
+                }
+            }
+            finally
+            {
+                if (lockAcquired)
+                {
+                    await lockService.UnLock(lockKey);
+                }
+            }
+        }
+    }
+}
