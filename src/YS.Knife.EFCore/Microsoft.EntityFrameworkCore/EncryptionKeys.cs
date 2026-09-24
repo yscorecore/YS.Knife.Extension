b4bd6c883ea17d3b741c7f992f50746901f1f5f0
yangsi <obpt@sina.com>
2026-09-24 10:32:01 +0800

    feat: EncryptionKeys 增加 LoadFromConfiguration 方法
    支持从 IConfiguration 批量加载加密密钥，便于在 ASP.NET Core 启动时初始化。

--- src/YS.Knife.EFCore/Microsoft.EntityFrameworkCore/EncryptionKeys.cs
+++ src/YS.Knife.EFCore/Microsoft.EntityFrameworkCore/EncryptionKeys.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.EntityFrameworkCore
 {
@@ -11,5 +12,17 @@ namespace Microsoft.EntityFrameworkCore
         public static string Get(string name) => _keys[name];
 
         public static bool TryGet(string name, out string key) => _keys.TryGetValue(name, out key);
+
+        public static void LoadFromConfiguration(IConfiguration configuration)
+        {
+            var encryptionKeys = configuration.GetSection(nameof(EncryptionKeys));
+            if (encryptionKeys != null)
+            {
+                foreach (var k in encryptionKeys.GetChildren())
+                {
+                    Set(k.Key, k.Value);
+                }
+            }
+        }
     }
 }
